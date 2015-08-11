d3ffbcb2d6eaebc6fe7858719b47be7fbcc12554
Travis Weston <tweston@bangordailynews.com>
2015-08-11 13:52:53 -0400

    Add GET/POST/DELETE/PUT methods, and make Options work.

--- examples/tlds.py
+++ examples/tlds.py
@@ -2,7 +2,7 @@ from wrAPy import Api
 
 class Jaundies(Api):
     def __getattr__(self, name):
-        return Api.call(self, name, 'http://api.jaundies.com/')
+        return Api.call(self, name, 'http://api.jaundies.com/', {'foo':'bar'})
 
 j = Jaundies()
 tlds = j.get
