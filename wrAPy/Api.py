64b45f35f33927e0b81b61f108d16e8905f0c527
Travis Weston <tweston@bangordailynews.com>
2015-08-17 11:01:34 -0400

    Revert "Add GET/POST/DELETE/PUT methods, and make Options work."
    This reverts commit d3ffbcb2d6eaebc6fe7858719b47be7fbcc12554.

--- wrAPy/Api.py
+++ wrAPy/Api.py
@@ -1,7 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import json
 import pycurl
-import urllib
 
 try:
     from io import BytesIO
@@ -11,60 +10,25 @@ except ImportError:
 class Api(object):
 	__metaclass__ = ABCMeta
 
-        def __init__(self):
-            self.customHeaders = []
-
         def call(self, name, path, options=None):
-            buffer = BytesIO()
-            c = pycurl.Curl()
-
-            def get(path, options):
-                if options is None:
-                    return path
-                return path + '?' + urllib.urlencode(options)
-
-            def post(options):
-                c.setopt(c.POST,True)
-                if options is None:
-                    return
-                setOptions(options)
-
-            def deletePut(name,options):
-                c.setopt(c.CUSTOMREQUEST, name.upper())
-                if options is None:
-                    return
-                setOptions(options)
-
-            def setOptions(options):
-                c.setopt(c.POSTFIELDS, options)
-
-            if name.lower() == "get":
-                path = get(path, options)
-        
-            elif name.lower() == "post":
-                post(options)
-
-            elif name.lower() == "put" or name.lower() == "delete":
-                deletePut(name, options)
-
-            c.setopt(c.URL, path)
-            c.setopt(c.WRITEDATA, buffer)
-
-            for i in range(len(self.customHeaders)):
-                c.setopt(c.HTTPHEADER,self.customHeaders[i])
-
-            c.perform()
-            c.close()
-
-            try:
-                result = json.load(buffer)
-
-            except ValueError:
-
+            def get(self, path, options=None):
+                buffer = BytesIO()
+                c = pycurl.Curl()
+                c.setopt(c.URL, path)
+                c.setopt(c.WRITEDATA, buffer)
+                c.perform()
+                c.close()
                 try:
-                    result = json.loads(buffer.getvalue())
-
+                    result = json.load(buffer)
                 except ValueError:
-                    result = None
+                    try:
+                        result = json.loads(buffer.getvalue())
+                    except ValueError:
+                        result = None
+                return result
+        
+            def post(self, path, options=None):
+                return False;
 
-            return result
+            if name == "get":
+                return get(self, path, options)
