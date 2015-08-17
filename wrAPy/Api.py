f6424aaa74ff5fc26214974a21aec4cc804a3b5f
Travis Weston <tweston@bangordailynews.com>
2015-08-17 11:27:21 -0400

    Rename Api submodule to Apis.

--- wrAPy/Api.py
+++ wrAPy/Api.py
@@ -1,34 +1,70 @@
 from abc import ABCMeta, abstractmethod
 import json
 import pycurl
+import urllib
 
 try:
     from io import BytesIO
 except ImportError:
     from StringIO import StringIO as BytesIO
 
-class Api(object):
+class Api:
 	__metaclass__ = ABCMeta
 
-        def call(self, name, path, options=None):
-            def get(self, path, options=None):
-                buffer = BytesIO()
-                c = pycurl.Curl()
-                c.setopt(c.URL, path)
-                c.setopt(c.WRITEDATA, buffer)
-                c.perform()
-                c.close()
-                try:
-                    result = json.load(buffer)
-                except ValueError:
-                    try:
-                        result = json.loads(buffer.getvalue())
-                    except ValueError:
-                        result = None
-                return result
-        
-            def post(self, path, options=None):
-                return False;
+        def __init__(self):
+            self.customHeaders = []
 
-            if name == "get":
-                return get(self, path, options)
+        def call(self, name, path, options=None):
+            buffer = BytesIO()
+            c = pycurl.Curl()
+
+            def get(path, options):
+                if options is None:
+                    return path
+                return path + '?' + urllib.urlencode(options)
+
+            def post(options):
+                c.setopt(c.POST,True)
+                if options is None:
+                    return
+                setOptions(options)
+
+            def deletePut(name,options):
+                c.setopt(c.CUSTOMREQUEST, name.upper())
+                if options is None:
+                    return
+                setOptions(options)
+
+            def setOptions(options):
+                c.setopt(c.POSTFIELDS, options)
+
+            if name.lower() == "get":
+                path = get(path, options)
+        
+            elif name.lower() == "post":
+                post(options)
+
+            elif name.lower() == "put" or name.lower() == "delete":
+                deletePut(name, options)
+
+            c.setopt(c.URL, path)
+            c.setopt(c.WRITEDATA, buffer)
+
+            for i in range(len(self.customHeaders)):
+                c.setopt(c.HTTPHEADER,self.customHeaders[i])
+
+            c.perform()
+            c.close()
+
+            try:
+                result = json.load(buffer)
+
+            except ValueError:
+
+                try:
+                    result = json.loads(buffer.getvalue())
+
+                except ValueError:
+                    result = None
+
+            return result
