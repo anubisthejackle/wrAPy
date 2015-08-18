37ba17d55984c10ac79d34f09b68f0639797efd5
Travis Weston <tweston@bangordailynews.com>
2015-08-18 09:35:48 -0400

    Add connect URL to Auth

--- wrAPy/Apis/Google/Auth.py
+++ wrAPy/Apis/Google/Auth.py
@@ -1,7 +1,11 @@
 from wrAPy import Api
+import urllib
 
 class Auth(Api):
     def __getattr__(self, name):
         def wrapper(path, options):
             return Api.call(self, name, 'https://accounts.google.com/o/oauth2' + path, options)
         return wrapper
+    # Returns the URL to redirect connect requests to
+    def connect(self, params):
+        return 'https://accounts.google.com/o/oauth2/auth?' + urllib.urlencode(options)
