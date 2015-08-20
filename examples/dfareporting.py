926b031a9fc027a9d1349443e8d503d8b5a11468
Travis Weston <tweston@bangordailynews.com>
2015-08-20 10:33:26 -0400

    Shorten DFAReporting example to only the necessary

--- examples/dfareporting.py
+++ examples/dfareporting.py
@@ -1,10 +1,7 @@
 from wrAPy.Apis.Google import DFAReporting
 import sys
 
-dfp = DFAReporting()
 profileId = sys.argv[1]
 
-result = dfp.get('/userprofiles/' + profileId + '/advertisers')
-
 print '########## ADVERTISERS FOR PROFILE ID ' + profileId + ' #########'
-print result
+print DFAReporting().get('/userprofiles/' + profileId + '/advertisers')
